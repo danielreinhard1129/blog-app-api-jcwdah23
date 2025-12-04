27066cda33d6b0b81cf6e8628af2989487223de9
danielreinhard1129 <danielreinhard1129@gmail.com>
2025-12-04 21:27:35 +0700

    feat: add login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -12,4 +12,9 @@ export class AuthController {
     const result = await this.authService.register(req.body);
     return res.status(200).send(result);
   };
+
+  login = async (req: Request, res: Response) => {
+    const result = await this.authService.login(req.body);
+    return res.status(200).send(result);
+  };
 }
