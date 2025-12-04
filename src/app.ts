9be053c0177f95fd66ff397fb4389cba9d0f80b2
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2025-12-04 19:24:18 +0700

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import "reflect-metadata";
+import cors from "cors";
+import express, { Express } from "express";
+import { PORT } from "./config/env";
+import { errorMiddleware } from "./middlewares/error.middleware";
+import { SampleRouter } from "./modules/sample/sample.router";
+import { ProductRouter } from "./modules/product/product.router";
+
+export class App {
+  app: Express;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  private configure() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  private routes() {
+    const sampleRouter = new SampleRouter();
+    const productRouter = new ProductRouter();
+
+    this.app.use("/samples", sampleRouter.getRouter());
+    this.app.use("/products", productRouter.getRouter());
+  }
+
+  private handleError() {
+    this.app.use(errorMiddleware);
+  }
+
+  public start() {
+    this.app.listen(PORT, () => {
+      console.log(`Server running on port : ${PORT}`);
+    });
+  }
+}
